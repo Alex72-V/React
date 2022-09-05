365518173ab51ccb292e759df7e1df39abdbdadf
Alex <lesnar1@mail.ru>
2022-09-05 16:44:25 +0300

    del_1_2

--- 1/src/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container');
-    const text = document.createElement('a');
-    text.style.color = 'green';
-    text.textContent = 'Рады приветствовать';
-    container.append(text)
-
-})

8fe3786f0803fff7c9f05fb38a04e8b7ac9b6d84
Alex <lesnar1@mail.ru>
2022-09-15 18:55:43 +0300

    lesson_7_done

--- react_007/src/redux/initialStore.js
+++ react_007/src/redux/initialStore.js
@@ -1,19 +1,21 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { createLogger } from "redux-logger";
+import { combineReducers, createStore } from "redux";
 import { chatReducer } from "./chatsReducer/chatReducer";
 import { messageReducer } from "./messagesReducer/messageReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
 const reducer = combineReducers({
     chats: chatReducer,
     messages: messageReducer
 })
 
-const logger = createLogger({
-
-    diff: true,
-    collapsed: true
-
-})
+const persisterReducer = persistReducer(persistConfig, reducer)
 
 
-export const store = createStore(reducer, applyMiddleware(logger));
+export const store = createStore(persisterReducer);
+export const persist = persistStore(store)

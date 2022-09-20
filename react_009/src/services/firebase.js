1e4d8eb769dd45a32695f990fd1c3d3e966267a0
Alex <lesnar1@mail.ru>
2022-09-20 19:03:27 +0300

    lesson_9_0

---
+++ react_009/src/services/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBHzrLH1gK-7zUSOY6yDCfLaDZK1N73rPs",
+    authDomain: "lesson9-aa929.firebaseapp.com",
+    projectId: "lesson9-aa929",
+    storageBucket: "lesson9-aa929.appspot.com",
+    messagingSenderId: "567465336089",
+    appId: "1:567465336089:web:ef8b71f7aa3948dabcf988"
+};
